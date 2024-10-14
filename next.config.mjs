cad85c84568dbc142401941f08c1a5ac46e71724
RASHID KHAN <rashidkhaan618@gmail.com>
2024-10-14 23:56:40 +0500

    alarm

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+export function webpack(config) {
+    config.module.rules.push({
+        test: /\.(mp3|wav)$/, 
+        use: {
+            loader: 'file-loader',
+            options: {
+                name: '[name].[ext]',
+                outputPath: 'static/media/', 
+                publicPath: '/_next/static/media/',
+            },
+        },
+    });
 
-export default nextConfig;
+    return config;
+}
+  
--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,197 @@
+"use client";
+
+import { useState, useEffect, useRef } from "react";
+import audioFile from "@/Assets/perfect_alarm.mp3";
 import Image from "next/image";
+import img from "@/Assets/giphy.gif";
 
-export default function Home() {
+function Home() {
+  const [currentTime, setCurrentTime] = useState("");
+  const [hour, setHour] = useState("");
+  const [min, setMin] = useState("");
+  const [timeZone, setTimeZone] = useState("pm");
+  const [error, setError] = useState("");
+  const [disable, setDisable] = useState(false);
+  const [alarmTime, setAlarmTime] = useState("");
+  const [alarmHour, setAlarmHour] = useState(0);
+  const [alarmMin, setAlarmMin] = useState(0);
+  const [alarmZone, setAlarmZone] = useState(0);
+  // const [AlarmAlert, setAlarmAlert] = useState(false);
+
+  const alarmAudio = useRef(null);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      getCurrentTime();
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  const handleAlarm = () => {
+    if (
+      alarmHour == currentTime.slice(0, 2) &&
+      alarmMin === currentTime.slice(5, 7) &&
+      alarmZone === currentTime.slice(8, 10)
+    ) {
+      alarmAudio.current.play();
+      // setAlarmAlert(true);
+      setTimeout(() => {
+        setAlarmTime(" ");
+        setDisable(false);
+      }, 20000);
+    }
+  };
+
+  const getCurrentTime = () => {
+    let getHour = new Date().getHours();
+    let getMinutes = new Date().getMinutes();
+    let time;
+
+    if (getHour > 12) {
+      time = `${getHour - 12 < 10 ? `0${getHour - 12}` : getHour - 12} : ${
+        getMinutes < 10 ? `0${getMinutes}` : getMinutes
+      } pm`;
+    } else {
+      time = `${getHour} : ${
+        getMinutes < 10 ? `0${getMinutes}` : getMinutes
+      } am`;
+    }
+    setCurrentTime(time);
+  };
+
+  // const handleStopAlarm = () => {
+  //   // alarmAudio.current.pause();
+  //   // setAlarmTime(" ");
+  //   // setDisable(false);
+  //   setAlarmAlert(false);
+  // };
+
+  const handleSetAlarm = () => {
+    if (!hour || hour < 1 || hour > 12) {
+      setError("Please enter a valid hour (1-12).");
+      // return;
+    } else if (!min || min < 0 || min >= 60) {
+      setError("Please enter a valid minute (0-59).");
+      return;
+    }else if (hour.match(/[a-zA-Z]/)) {
+      setError("Hour should be a number.");
+      return;
+    } else if (min.match(/[a-zA-Z]/)) {
+      setError("Minute should be a number.");
+      return;
+    }else {
+      setHour("");
+      setMin("");
+      setTimeZone("");
+      setError("");
+      setAlarmTime(`Alarm set for: ${hour}:${min} ${timeZone}`);
+      setAlarmHour(hour);
+      setAlarmMin(min);
+      setAlarmZone(timeZone);
+      setDisable(true);
+    }
+  };
+  handleAlarm();
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+      <div className="h-screen w-screen flex justify-center items-center flex-col overflow-hidden">
+        <div className="h-[65vh] w-[95%] mx-2 md:w-[35vw] px-2 flex justify-center items-center flex-col">
+          <div className="currentTime mb-10">
+            <h1 className="text-4xl mb-10">Current Time</h1>
+            <p className="text-3xl text-center">{currentTime}</p>
+          </div>
+          <div className="line w-[97%] md:w-[400px] h-1 rounded-full bg-slate-50"></div>
+          <div className="alarm mt-10">
+            <h1 className="text-4xl mb-5 text-center">Set Alarm</h1>
+            <div className="text-center mb-5 font-semibold">{alarmTime}</div>
+            <div className="flex justify-center text-black">
+              <input
+                autoFocus
+                onKeyUp={(e) => {
+                  if (e.key === "Enter") {
+                    document.querySelector("#input_2").focus();
+                  }
+                }}
+                type="text"
+                value={hour}
+                onChange={(e) => setHour(e.target.value)}
+                placeholder="Enter Hour"
+                className="outline-none bg-slate-300 me-3 border-2 border-gray-300 p-2 w-[27vw] md:w-40 rounded-md"
+              />
+              <input
+                id="input_2"
+                type="text"
+                value={min}
+                onChange={(e) => {
+                  setMin(e.target.value);
+                }}
+                onKeyUp={(e) => {
+                  if (e.key === "Enter") {
+                    document.querySelector("button").focus();
+                  }
+                }}
+                placeholder="Enter Minutes"
+                className="rounded-md bg-slate-300 outline-none border-2 border-gray-300 p-2 w-[27vw] md:w-40"
+              />
+              <select
+                name="timeZone"
+                id="timeZone"
+                value={timeZone}
+                onChange={(e) => setTimeZone(e.target.value)}
+                className="p-2 w-[16vw] md:w-16 h-10 mt-[2px] ms-3 rounded-md outline-none bg-slate-300"
+              >
+                <option value="pm">pm</option>
+                <option value="am">am</option>
+              </select>
+            </div>
+            {error && (
+              <p className="text-red-500 text-2xl text-center mt-3">{error}</p>
+            )}
+            {disable ? (
+              <button className="mt-5 bg-blue-500 hover:bg-blue-600 text-black ml-7 md:ml-16 text-2xl w-[250px] h-16 rounded-full">
+                Set Alarm
+              </button>
+            ) : (
+              <button
+                className="mt-5 bg-blue-500 hover:bg-blue-600 text-black ml-7 md:ml-16 text-2xl w-[250px] h-16 rounded-full"
+                onClick={handleSetAlarm}
+                onKeyUp={(e) => {
+                  if (e.key === "Enter") {
+                    document.querySelector("button").click();
+                  }
+                }}
+              >
+                Set Alarm
+              </button>
+            )}
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+        <audio ref={alarmAudio} src={audioFile} preload="auto"></audio>
+        {AlarmAlert && (
+          <div className="w-[50%] h-[60%] flex flex-col justify-center items-center bg-slate-600 absolute rounded-lg">
+            <h1 className="text-4xl text-center text-black">Wake Up Dear !</h1>
+            <div className="alarmGIF w-[30%] h-[40%] mb-16 ">
+              <Image
+                src={img}
+                width={1000}
+                height={1000}
+                alt="Picture of the alarm"
+              />
+            </div>
+            <button
+              type="button"
+              className="w-[20%] h-[10%] bg-red-600 rounded-md hover:bg-red-700 px-2"
+              // onClick={handleStopAlarm}
+            >
+              Stop
+            </button>
+          </div>
+        )}
+      </div>
+    </>
   );
 }
+
+export default Home;
